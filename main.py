45b39f447705d1eecd698dc6ff9cde31e0bbf2cd
rinat-proff <rinat-proff@mail.ru>
2024-05-24 20:21:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+print('a')
+print(len("a"))
+first = 3
+second = 5
+print(first + second)
+print(second - first)
+summa = 8
+diff = 2
+print(type(8))
+print(type(2))
+print(8)
+print(2)
+first = 5
+second = 7
+third = 9
+print((5 + 7 + 9) // 3)
+mean = 7
+print(7)
+first_string = "вторник"
+second_string = "понедельник"
+name1 = "вторник"
+name2 = "понедельник, "
+print(name2 + name1)                   # первый вариант
+print("понедельник" "," " вторник")    # второй вариант
+a = 1
+b = 2
+c = 3
+print((a * b) + (a * c))
+f = 5
+print((5 ** 3) / 2)
+
+
+
